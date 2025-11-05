f6b421e244d8054d6e1083ac864bd5df99a9cf31
myproteinshaker <nk66ve@student.aau.dk>
2025-11-05 15:37:34 +0100

    Update robot_programmering.ino

--- Zumo_maze_solver/Solve_Dan/robot_programmering/robot_programmering.ino
+++ Zumo_maze_solver/Solve_Dan/robot_programmering/robot_programmering.ino
@@ -1,108 +1,106 @@
-#include <Wire.h> // inkluderer bibliotek
-#include <Zumo32U4.h> // inkluderer bibliotek
+#include <Wire.h>      // inkluderer bibliotek
+#include <Zumo32U4.h>  // inkluderer bibliotek
 
-Zumo32U4Motors motors; // inkluderer motorer
-Zumo32U4LineSensors lineSensors; // inkluderer 
-Zumo32U4OLED OLED; // inkluderer OLED-skærm
+Zumo32U4Motors motors;            // inkluderer motorer
+Zumo32U4LineSensors lineSensors;  // inkluderer
+Zumo32U4OLED OLED;                // inkluderer OLED-skærm
 
-#define NUM_SENSORS 3 // definerer antal sensorer
-unsigned short lineSensorValues[NUM_SENSORS]; // gemmer målingerne i en array
-unsigned short direction = 0; // retning - bruges til cases
+#define NUM_SENSORS 3                          // definerer antal sensorer
+unsigned short lineSensorValues[NUM_SENSORS];  // gemmer målingerne i en array
+int Direction = 1;                             // retning - bruges til cases
+int subDirection = 2;                          // retning - bruges til cases
 
-//                                 L     C     R   
+//                                 L     C     R
 //Hvis det er hvid er det under = 300 , 300 , 300
 
-unsigned short speed = 100; // sætter hastighed (unsigned short = 0 til 65535)
+unsigned short speed = 100;  // sætter hastighed (unsigned short = 0 til 65535)
 
 void printReadingsToSerial() {
-  char buffer[80]; // opretter tegn-array med plads til 80 tegn
-  sprintf(buffer, "%4d %4d %4d\n", lineSensorValues[0], lineSensorValues[1], lineSensorValues[2]); // i stedet for Serial.print(), da sprintf() er pæn og i en streng
-  Serial.print(buffer); // print strengen
+  char buffer[80];                                                                                  // opretter tegn-array med plads til 80 tegn
+  sprintf(buffer, "%4d %4d %4d\n", lineSensorValues[0], lineSensorValues[1], lineSensorValues[2]);  // i stedet for Serial.print(), da sprintf() er pæn og i en streng
+  Serial.print(buffer);                                                                             // print strengen
 }
 
 void readLineSensors() {
-  lineSensors.read(lineSensorValues, QTR_EMITTERS_ON); // læser sensorerne og gemmer værdierne i arrayet [NUM_SENSORS]
+  lineSensors.read(lineSensorValues, QTR_EMITTERS_ON);  // læser sensorerne og gemmer værdierne i arrayet [NUM_SENSORS]
 }
 
 void forward() {
-  motors.setSpeeds(speed,speed); // kør med defineret hastighed
+  motors.setSpeeds(speed, speed);  // kør med defineret hastighed
 }
 
 void backwards() {
-  motors.setSpeeds(-speed,-speed); // kør baglæns med defineret hastighed
+  motors.setSpeeds(-speed, -speed);  // kør baglæns med defineret hastighed
 }
 
 void turnLeft() {
-  motors.setSpeeds(-speed,speed); // drej til venstre med defineret hastighed
+  motors.setSpeeds(-speed, speed);  // drej til venstre med defineret hastighed
 }
 
 void turnRight() {
-  motors.setSpeeds(speed,-speed); // drej til højre med defineret hastighed
+  motors.setSpeeds(speed, -speed);  // drej til højre med defineret hastighed
 }
 
 void stop() {
-  motors.setSpeeds(0,0); // stop
+  motors.setSpeeds(0, 0);  // stop
 }
 
 void chooseDirection() {
-  if (lineSensorValues[0]>300) { // hvis venstre sensor ser linjen = følg venstre
-    direction = 1;
-  }
-  else if (lineSensorValues[2]>300) { // hvis højre sensor ser linjen = følg højre
-    direction = 2;
+  motors.setSpeeds(speed, speed);
+  while (Direction == 1) {
+    readLineSensors();
+    if (lineSensorValues[0] > 300) {  // hvis venstre sensor ser linjen = følg venstre
+      Direction = 0;
+      subDirection = 2;
+    } else if (lineSensorValues[2] > 300) {  // hvis højre sensor ser linjen = følg højre
+      Direction = 2;
+      subDirection = 0;
+    }
   }
 }
 
-void followLeft() {
-    while (lineSensorValues[0]>300 && lineSensorValues[1]<=300 && lineSensorValues[2]<=300) {
-      forward(); // så længe kun venstre sensor ser linjen: kør
-      readLineSensors();
-    }
-    while (lineSensorValues[0]<300) {
-      turnLeft(); // så længe venstre sensor ikke ser linjen: drej til venstre
-      readLineSensors();
-    }
-
-    while (lineSensorValues[1]>300||lineSensorValues[2]>300) {
-      turnRight(); // så længe midt eller højre sensor ser linje: drej til højre
-      readLineSensors();
-    }
-}
-
-void followRight() {
-  while (lineSensorValues[0]<=300 && lineSensorValues[1]<=300 && lineSensorValues[2]>300) {
-    forward(); // så længe kun højre sensor ser linje: kør
+void followDirection() {
+  while (lineSensorValues[Direction] > 500 && lineSensorValues[1] <= 300 && lineSensorValues[subDirection] <= 300) {
+    forward();  // så længe kun venstre sensor ser linjen: kør
+    readLineSensors();
+  }
+  while (lineSensorValues[Direction] < 300) {
+    if (Direction == 0) turnLeft();  // så længe venstre sensor ikke ser linjen: drej til venstre
+    else if (Direction == 2) turnRight();
     readLineSensors();
   }
 
-  while (lineSensorValues[2]<300) {
-    turnRight(); // så længe højre sensor ikke ser linjen: drej til højre
-    readLineSensors();
-  }
-
-  while (lineSensorValues[0]>300||lineSensorValues[1]>300) {
-    turnLeft(); // så længe venstre eller midt sensor ser linje: drej til venstre
+  while (lineSensorValues[1] > 300 || lineSensorValues[subDirection] > 300) {
+    if (Direction == 0) turnRight();  // så længe midt eller højre sensor ser linje: drej til højre
+    else if (Direction == 2 && lineSensorValues[subDirection] > 300) turnLeft();
+    else if (Direction == 2 && lineSensorValues[Direction] > 300) motors.setSpeeds(100,0);
     readLineSensors();
   }
 }
 
 void setup() {
-  lineSensors.initThreeSensors(); // initialiserer de tre sensorer
+  lineSensors.initThreeSensors();  // initialiserer de tre sensorer
   Serial.begin(9600);
+  chooseDirection();  // vælg retning
 }
 
 void loop() {
-  readLineSensors(); // læs sensorerne
-  printReadingsToSerial(); // print til serial monitor
-  chooseDirection(); // vælg retning
-  switch(direction) {
-  case 1: 
+  readLineSensors();        // læs sensorerne
+  printReadingsToSerial();  // print til serial monitor
+  switch (Direction) {
+    case 0:
+      followDirection();
+      break;
+    case 2:
+      followDirection();
+      break;
+    /*case 1: 
     followLeft();
     break;
   case 2: 
     followRight();
-    break;
-  default:
-    forward(); // hvis ingen linje er fundet: kør lige ud
+    break;*/
+    default:
+      forward();  // hvis ingen linje er fundet: kør lige ud
   }
 }
