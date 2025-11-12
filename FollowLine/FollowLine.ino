bc732459dec0199a0b8d7072c2a7a1efde507cee
myproteinshaker <nk66ve@student.aau.dk>
2025-11-12 12:09:29 +0100

    Update FollowLine.ino

--- FollowLine/FollowLine.ino
+++ FollowLine/FollowLine.ino
@@ -2,7 +2,7 @@
 #include <Zumo32U4.h>  // inkluderer bibliotek
 
 Zumo32U4Motors motors;            // inkluderer motorer
-Zumo32U4LineSensors lineSensors;  // inkluderer
+Zumo32U4LineSensors lineSensors;  // inkluderer sensorer
 Zumo32U4OLED OLED;                // inkluderer OLED-skærm
 
 #define NUM_SENSORS 3                          // definerer antal sensorer
@@ -12,39 +12,7 @@ unsigned char speed = 100;  // sætter hastighed (unsigned short = 0 til 65535)
 
 unsigned short hvid = 500;
 
-unsigned char a;
-unsigned char b;
-unsigned char c;
-unsigned char d;
-unsigned char e;
-unsigned char f;
-
-unsigned short readLine;
-unsigned int * sensor_values;
-unsigned char readMode = QTR_EMITTERS_ON;
-unsigned char white_line = 0;
-
-void printReadingsToSerial() {
-  char buffer[80];                                                                               // opretter tegn-array med plads til 80 tegn
-  sprintf(buffer,                                                                                // i stedet for Serial.print(), da sprintf() er pæn og i en streng
-          "%4d %4d | %4d %4d | %4d %4d     %4d | %4d | %4d     %3d %3d | %3d %3d | %3d %3d | %8d \n",  // %d = udskriv et helt tal, 4 = mindst fire karakterer (da 2000 er max), \n = linjeskift
-          lineSensors.calibratedMinimumOn[0],
-          lineSensors.calibratedMaximumOn[0],
-          lineSensors.calibratedMinimumOn[1],
-          lineSensors.calibratedMaximumOn[1],
-          lineSensors.calibratedMinimumOn[2],
-          lineSensors.calibratedMaximumOn[2],
-          lineSensorValues[0],
-          lineSensorValues[1],
-          lineSensorValues[2],
-          a, b, c, d, e, f),
-          lineSensors.readLine(lineSensorValues);
-  Serial.print(buffer);  // print strengen
-}
-
-void readLineSensors() {
-  lineSensors.read(lineSensorValues, QTR_EMITTERS_ON);  // læser sensorerne og gemmer værdierne i arrayet [NUM_SENSORS]
-}
+unsigned char a, b, c, d, e, f;
 
 void forward() {
   motors.setSpeeds(speed, speed);  // kør med defineret hastighed
@@ -66,25 +34,6 @@ void stop() {
   motors.setSpeeds(0, 0);  // stop
 }
 
-void followLine() {
-  while (lineSensorValues[1] > hvid && lineSensorValues[0] <= hvid && lineSensorValues[2] <= hvid) {
-    forward();  // så længe kun venstre sensor ser linjen: kør
-    readLineSensors();
-  }
-
-  while (lineSensorValues[0] > hvid || lineSensorValues[2] > hvid) {
-    if (lineSensorValues[0] > hvid) {
-      turnLeft();
-      readLineSensors();
-    }
-    // så længe venstre sensor ikke ser linjen: drej til venstre
-    else if (lineSensorValues[2] > hvid) {
-      turnRight();
-      readLineSensors();
-    }
-  }
-}
-
 void calibrateSensors() {
   lineSensors.calibrate();  // gemmer min-og max-værdier for hver sensor fra arrayet [NUM_SENSORS] i lineSensors.calibratedMinimum[NUM_SENSORS] og lineSensors.calibratedMaximum[NUM_SENSORS]
 }
@@ -98,15 +47,49 @@ void normalize() {
   f = lineSensors.calibratedMaximumOn[2] / 2000.0 * 100;
 }
 
+void printReadingsToSerial() {
+  char buffer[120];                                                                                      // opretter tegn-array med plads til 80 tegn
+  sprintf(buffer,                                                                                        // i stedet for Serial.print(), da sprintf() er pæn og i en streng
+          "%4d %4d | %4d %4d | %4d %4d     %4d | %4d | %4d     %3d %3d | %3d %3d | %3d %3d     %4d \n",  // %d = udskriv et helt tal, 4 = mindst fire karakterer (da 2000 er max), \n = linjeskift
+          lineSensors.calibratedMinimumOn[0],
+          lineSensors.calibratedMaximumOn[0],
+          lineSensors.calibratedMinimumOn[1],
+          lineSensors.calibratedMaximumOn[1],
+          lineSensors.calibratedMinimumOn[2],
+          lineSensors.calibratedMaximumOn[2],
+          lineSensorValues[0],
+          lineSensorValues[1],
+          lineSensorValues[2],
+          a, b, c, d, e, f,
+          lineSensors.readLine(lineSensorValues));
+  Serial.print(buffer);
+}
+
+void readLineSensors() {
+  lineSensors.read(lineSensorValues, QTR_EMITTERS_ON);  // læser sensorerne og gemmer værdierne i arrayet [NUM_SENSORS]
+}
+
+void FollowLine() {
+ unsigned short position = lineSensors.readLine(lineSensorValues);
+ int error = position - 1000;
+ if (position == 0 || position == 2000) {
+  error = 0;
+ }
+ float Kp = 0.3;
+ float Up = Kp * error;
+ int leftMotorSpeed  = constrain(speed + Up, 0, 100);
+ int rightMotorSpeed = constrain(speed - Up, 0, 100);
+ motors.setSpeeds(leftMotorSpeed, rightMotorSpeed);
+}
+
 void setup() {
   lineSensors.initThreeSensors();  // initialiserer de tre sensorer
   Serial.begin(9600);
 }
 
 void loop() {
-  readLineSensors();
   calibrateSensors();
   normalize();
   printReadingsToSerial();
-  followLine();
+  FollowLine();
 }
